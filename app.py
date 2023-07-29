db165b23343b7319c5ba827c19219e526d58ca0b
weather <ajay.podicheti20@gmail.com>
2023-07-29 19:10:28 +0100

    added time zone london

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 # save this as app.py
 from flask import Flask
+import requests
 
 app = Flask('Flask')
 
 @app.route("/")
-def hello():
-    return "Hello!"
+def meteo():
+    meteo_Api = "https://api.open-meteo.com/v1/forecast?"
+    params = {
+        'latitude': '52.52',
+        'longitude': '13.41',
+         'timezone': 'Europe/London'
+    }
+    output = requests.get(meteo_Api, params=params, verify=False)
+    return output.json()
+    
